8c3f37fec0c375dc3b9520544a1db169fda7d6f7
Marek Tutaj <mtutaj@mcw.edu>
2018-09-21 13:34:17 -0500

    fixed scripts

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -3,12 +3,12 @@
 # phenotype annotation loading pipeline
 #
 . /etc/profile
-APPNAME=annotationImport
+APPNAME=PhenotypeAnnotation
 
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
 pwd
 DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
 LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-declare -x "ANNOTATION_IMPORT_OPTS=$DB_OPTS $LOG4J_OPTS"
+declare -x "PHENOTYPE_ANNOTATION_OPTS=$DB_OPTS $LOG4J_OPTS"
 bin/$APPNAME "$@"
